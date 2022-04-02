2b665b914a8325ec16b51edc93dcf0ef9645ee48
poirot-en-bardo <balica.eliza@gmail.com>
2022-04-02 13:42:22 +0300

    BE: added Spring project

---
+++ art-assistant-be/src/main/java/ie/licenta/artassistant/ArtAssistantApplication.java
@@ -0,0 +1,13 @@
+package ie.licenta.artassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtAssistantApplication.class, args);
+	}
+
+}
---
+++ art-assistant-be/src/test/java/ie/licenta/artassistant/ArtAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.licenta.artassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

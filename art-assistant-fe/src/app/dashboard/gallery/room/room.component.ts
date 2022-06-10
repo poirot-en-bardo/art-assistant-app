47369cdb4b14aec7b9235fa7c432d4c3e41b17c6
poirot-en-bardo <balica.eliza@gmail.com>
2022-06-10 16:08:17 +0300

    FE: Artwork carousel

--- art-assistant-fe/src/app/dashboard/gallery/room/room.component.ts
+++ art-assistant-fe/src/app/dashboard/gallery/room/room.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {BaseComponent} from "../../../core/components/base/base.component";
 import {RoomService} from "../../../shared/services/room.service";
 import {ArtworkService} from "../../../shared/services/artwork.service";
@@ -6,7 +6,7 @@ import {ActivatedRoute} from "@angular/router";
 import {RoomModel} from "../../../shared/models/room.model";
 import {ArtworkModel} from "../../../shared/models/artwork.model";
 import {takeUntil} from "rxjs";
-import { ImageUtils } from '../../../shared/utils/image.utils';
+import {ImageUtils} from '../../../shared/utils/image.utils';
 
 @Component({
   selector: 'app-room',
@@ -17,6 +17,7 @@ export class RoomComponent extends BaseComponent implements OnInit {
 
   room: RoomModel;
   artworks: ArtworkModel[];
+  index: number;
 
   constructor(private roomService: RoomService, private artworkService: ArtworkService,
               private route: ActivatedRoute) {
@@ -24,6 +25,7 @@ export class RoomComponent extends BaseComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.index = 0;
     this.getData();
   }
 
@@ -41,13 +43,28 @@ export class RoomComponent extends BaseComponent implements OnInit {
     this.artworkService.getArtworksByRoomId(this.room.id).pipe(takeUntil(this.unsubscribe$))
       .subscribe(artworks => {
         this.artworks = artworks;
-        this.artworks.forEach(artwork =>{
-          if(artwork.imagePath !== null)
-            artwork.imagePath = ImageUtils.appendImageType(artwork.imagePath);
+        this.artworks.forEach(artwork => {
+            if (artwork.imagePath !== null)
+              artwork.imagePath = ImageUtils.appendImageType(artwork.imagePath);
           }
         )
         console.log(artworks);
       })
+  }
+
+  prev() {
+    if (this.index < this.artworks.length - 1) {
+      this.index++;
+    }
+    console.log(this.index);
+  }
+
+  next() {
+    if (this.index > 0) {
+      this.index--;
+    }
+    console.log(this.index);
+
   }
 
 }
